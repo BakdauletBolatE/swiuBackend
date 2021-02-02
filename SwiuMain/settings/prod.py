460e02b9023243f30fe14aac5723d566822e329e
Bakdaulet <bakosh21345@gmail.com>
2021-02-02 21:14:40 +0600

    s

--- SwiuMain/settings/prod.py
+++ SwiuMain/settings/prod.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4_ef3a3_mnty-ngd9%tur+h+q@ckwbwk9ktk7hsjdw4%g3@3hl'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
