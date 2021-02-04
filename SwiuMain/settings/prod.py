9cc5a76ad81137818d5e843472f35b0b86925168
Bakdaulet <Bakosh21345@gmail.com>
2021-02-04 14:17:17 +0600

    final

--- SwiuMain/settings/prod.py
+++ SwiuMain/settings/prod.py
@@ -84,7 +84,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 LANGUAGE_CODE = 'ru'
 
