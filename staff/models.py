e82dc1ad1dd4d0ac2d488c6cd414f62d68749b82
Bakdaulet <Bakosh21345@gmail.com>
2021-02-12 12:22:22 +0600

    fix joinstaff

--- staff/models.py
+++ staff/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from departments.models import Department,EducationalPrograms
+from faculties.models import Facult
 
 # Create your models here.
 class Position(models.Model):
@@ -33,10 +34,11 @@ class Staff(models.Model):
     position = models.ForeignKey(Position,on_delete=models.CASCADE,blank=True,null=True)
     staffCat = models.ForeignKey(StaffCat,on_delete=models.CASCADE,blank=True,null=True)
     department = models.ForeignKey(Department,on_delete=models.CASCADE,blank=True,null=True)
-    educationalPrograms = models.ManyToManyField(EducationalPrograms,blank=True,null=True)
+    educationalPrograms = models.ManyToManyField(EducationalPrograms)
     email = models.EmailField('Почта персонала',blank=True,null=True,max_length=255)
     adress = models.CharField('Адрес персонала',blank=True,null=True,max_length=255)
     phone = models.CharField('Телефон персонала',blank=True,null=True,max_length=255)
+    facult = models.ForeignKey(Facult,on_delete=models.CASCADE)
     about = models.TextField('О персонале')
 
     class Meta:
