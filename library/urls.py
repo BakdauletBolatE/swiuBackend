31ab90786e77a7bb0603236626d7f25a4126720c
Bakdaulet <bakosh21345@gmail.com>
2021-02-28 17:43:52 +0600

    all comitted

--- library/urls.py
+++ library/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('',libindex,name="libindex"),
+    path('index/',libindex,name="libindex"),
     path('book/<slug:url>',bookList,name="bookList"),
     path('<slug:url>/',bookDetail,name="bookDetail")
 ]
