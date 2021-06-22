0b7c2b2d2fa3deeaa948e1c02aeb9c5196ad71b8
bakdaulet <bakosh21345@gmail.com>
2021-06-22 12:26:55 +0600

    a

--- departments/urls.py
+++ departments/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('<slug:url>',departmentDetailView,name="departmentDetailView"),
+    path('<slug:url>', departmentDetailView, name="departmentDetailView"),
     path('edu/<slug:url>',eduProgramDetail,name="eduProgDetailView")
 ] 
  
